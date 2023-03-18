599cf5553bbec6f6298660fc5a211a0f42a3bc6f
Zeeshan Haider <z.haideraspirant@gmail.com>
2023-03-18 22:48:36 +0500

    To run directly from Github

---
+++ app.R
@@ -0,0 +1,6 @@
+
+pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
+options( "golem.app.prod" = TRUE)
+NigeriaDisasterRisks::run_app() # add parameters here (if any)
+
+
